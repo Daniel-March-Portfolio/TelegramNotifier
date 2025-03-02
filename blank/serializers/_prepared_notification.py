f926b44c568a2ec6a8bcec6b48d2d9312a594efb
Daniel-March <town.kor@gmail.com>
2025-03-03 01:29:25 +0300

    template_variables no longer required in request

--- blank/serializers/_prepared_notification.py
+++ blank/serializers/_prepared_notification.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class PreparedNotificationSerializer(serializers.Serializer):
     blank_tag = serializers.CharField(max_length=64)
-    template_variables = serializers.DictField()
+    template_variables = serializers.DictField(required=False, default={})
--- notification/serializers/_notification.py
+++ notification/serializers/_notification.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 
 class NotificationSerializer(serializers.Serializer):
     template_tag = serializers.CharField(max_length=64)
-    template_variables = serializers.DictField()
+    template_variables = serializers.DictField(required=False, default={})
     bot_uuid = serializers.UUIDField()
     chat_id = serializers.IntegerField()
